83e0078e23eb2a5d8f1d8130f7e90ab313f61ad3
Jeonha Kang <kangjeonha@gmail.com>
2024-07-14 21:18:27 +0900

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Homepage from './page/Homepage';
 import Aboutpage from './page/Aboutpage';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Productpage from './page/Productpage';
 import ProductDetailpage from './page/ProductDetailpage';
+import Loginpage from './page/Loginpage';
+import Userpage from './page/Userpage';
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(true)
+  const PrivateRoute = () => {
+    return authenticate == true ? <Userpage /> : <Navigate to="/login" />
+  }
   return (
     <div>
       <Routes>
@@ -14,6 +20,8 @@ function App() {
         <Route path="/about" element={<Aboutpage />}></Route>
         <Route path="/products" element={<Productpage />}></Route>
         <Route path="/products/:id" element={<ProductDetailpage />}></Route>
+        <Route path="/login" element={<Loginpage />}></Route>
+        <Route path="/user" element={<PrivateRoute />}></Route>
       </Routes>
     </div >
   );
--- src/page/Productpage.js
+++ src/page/Productpage.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const Productpage = () => {
+    let [query, setQuery] = useSearchParams()
+    console.log(query.get('page'))
     return (
         <div>
             <h1>Show All Products</h1>
